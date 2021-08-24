8dc06570e40ce8bc9604673a97d05b1b128b500d
veli.yetisgengil <yetisgengil_veli@hotmail.com>
2021-08-25 00:42:17 +0300

    ExceptionHandler All Method Contains

---
+++ VY.Asp.Net.Core.Middleware/Controllers/RandomController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VY.Asp.Net.Core.Middleware.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomController : ControllerBase
+    {
+     
+
+        [HttpGet]
+        public String Get()
+        {
+            Random random = new Random();
+
+            int number = random.Next(0, 100);
+            
+            if(number > 50)
+            {
+                return "Congratulations !!! You Are Pass Class :)";
+            }
+            else
+            {
+                throw new Exception("Sorry You didn't pass the class :(");
+            }
+        }
+    }
+}
---
+++ VY.Asp.Net.Core.Middleware/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VY.Asp.Net.Core.Middleware.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate Next,ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            next = Next;
+            this.logger = logger;
+        }
+
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await next.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+                //Exception Handle Write Debug
+                 logger.LogError(ex.Message);
+            }
+        }
+        
+    }
+}
